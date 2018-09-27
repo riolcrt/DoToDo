fd5e5f754b713aef0af2a63b39d39521f03089d8
Rolan Gonzalez <riolcrt@gmail.com>
2018-09-27 20:57:53 +0200

    ✔ Generar la estructura general de la aplicación. @started(18-09-27 19:30) @done(18-09-27 20:36) @lasted(1h6m35s)

--- Dotodo/src/app/text-editor/text-editor.component.ts
+++ Dotodo/src/app/text-editor/text-editor.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-text-editor',
   templateUrl: './text-editor.component.html',
   styleUrls: ['./text-editor.component.css']
 })
-export class TextEditorComponent implements OnInit {
+export class TextEditorComponent {
+  constructor (private state: Store) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  onTextInput (e: TextEvent) {
+    const target: HTMLTextAreaElement = e.target as HTMLTextAreaElement;
+    // this.state.dispatch(new UpdateText(target.value));
   }
-
 }
