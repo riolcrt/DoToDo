401049a8275d76491c8abf78666e9cbab1ef3d2b
Rolan Gonzalez <riolcrt@gmail.com>
2018-09-28 18:23:46 +0200

    bindear todos los atajos de teclado

--- Dotodo/src/app/text-editor/text-editor.component.ts
+++ Dotodo/src/app/text-editor/text-editor.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { UpdateText } from '../shared/TodosActions';
-import { Observable } from 'rxjs';
+import { Observable, fromEvent} from 'rxjs';
+import { map, filter, takeWhile } from 'rxjs/operators';
 import { TodoItem, TodoItemTypeEnum } from '../shared/TodosModel';
 import { TodosState } from '../shared/TodosState';
 
@@ -14,9 +15,7 @@ export class TextEditorComponent implements OnInit {
   public todoType: typeof TodoItemTypeEnum = TodoItemTypeEnum;
   public todos$: Observable<TodoItem[]> = this.state.select(TodosState.GetTodoItems);
 
-  constructor (private state: Store, private el: ElementRef) {
-    this.todos$.subscribe(x => console.log ('recibido mediante subscripcion', x));
-  }
+  constructor (private state: Store, private el: ElementRef) {}
 
   ngOnInit() {
     const textArea: HTMLTextAreaElement = this.el.nativeElement.querySelector('textarea');
@@ -26,6 +25,30 @@ export class TextEditorComponent implements OnInit {
   onTextInput (e: TextEvent) {
     const target: HTMLTextAreaElement = e.target as HTMLTextAreaElement;
     this.dispatchUpdate(target.value);
+  }
+
+  onKeydown(e: KeyboardEvent ) {
+    if (e.altKey || e.code === 'Tab') {
+      switch (e.key) {
+        case 'Tab':
+          if (e.shiftKey) {
+            console.log ('decrease indent');
+            break;
+          }
+          console.log ('increase indent');
+          break;
+        case 'd':
+          console.log ('set as done');
+          break;
+        case 's':
+          console.log ('set as started');
+          break;
+        case 'c':
+          console.log ('set as cancelled');
+          break;
+      }
+      e.preventDefault();
+    }
   }
 
   dispatchUpdate(payload: string) {
